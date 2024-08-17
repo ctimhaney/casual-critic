cfb5e0e63298a15332208f4651516d8c7a4084e8
ctimhaney <colinthaney@gmail.com>
2024-08-17 15:57:17 -0500

    feat: bump jackson and json versions to latest (#2)

---
+++ dependencies.sh
@@ -0,0 +1,10 @@
+curl https://repo1.maven.org/maven2/com/fasterxml/jackson/core/jackson-databind/2.17.2/jackson-databind-2.17.2.jar \
+  -o lib/jackson-databind-2.17.2.jar
+curl https://repo1.maven.org/maven2/org/json/json/20240303/json-20240303.jar \
+  -o lib/json-20240303.jar
+curl https://repo1.maven.org/maven2/org/springframework/boot/spring-boot-starter-web/3.3.2/spring-boot-starter-web-3.3.2.jar \
+  -o lib/spring-boot-starter-web-3.3.2.jar
+curl https://repo1.maven.org/maven2/com/fasterxml/jackson/core/jackson-core/2.17.2/jackson-core-2.17.2.jar \
+  -o lib/jackson-core-2.17.2.jar
+curl https://repo1.maven.org/maven2/com/fasterxml/jackson/core/jackson-annotations/2.17.2/jackson-annotations-2.17.2.jar \
+  -o lib/jackson-annotations-2.17.2.jar
